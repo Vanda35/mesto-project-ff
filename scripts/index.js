14d5ef8826428faeaf6c5936c95fdb22784c7097
Daria Rodikova <79627063268@yandex.ru>
2024-03-26 20:49:31 +0300

    function createCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const templateCard = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function createCard(item) {
+  const cardElement = templateCard
+    .querySelector(".places__item")
+    .cloneNode(true);
+  cardElement.querySelector(".card__image").src = item.link;
+  cardElement.querySelector(".card__title").textContent = item.name;
 
-// @todo: Функция создания карточки
+  deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", function (evt) {
+    deleteCard(evt.target);
+  });
 
-// @todo: Функция удаления карточки
+  return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+function deleteCard(currentButton) {
+  console.log(currentButton);
+  currentElement = currentButton.closest(".places__item");
+  currentElement.remove();
+}
+
+initialCards.forEach(function (item) {
+  placesList.append(createCard(item));
+});
